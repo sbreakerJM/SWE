f96dc8a690848f9344ade68501fc3d1d0c3f4e63
sbreakerJM <bs7517@mci4me.at>
2025-03-28 13:13:22 +0100

    Anlegen Ordnerstruktur

--- main.py
+++
@@ -1,9 +0,0 @@
-if  __name__ =="__main__":
-    
-    from my_functions import build_person, build_experiment
-    supervisor = build_person("Walter", "White", "Male", 50)
-    subject = build_person("Jesse", "Pinkman", "Male", 27)
-    experiment = build_experiment("Experiment 1", "2025-03-21", supervisor, subject)
-
-    print(experiment)
-    print("Test1")
